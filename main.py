22d47021b84d3e062fb15eb6d9dd82ceaf5bd1da
FarkhadTut <Farkhadnavoi@yandex.com>
2022-07-29 15:01:59 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+con = psycopg2.connect(
+    host='ec2-3-248-121-12.eu-west-1.compute.amazonaws.com',
+    database='d1ocevteqdh8vf',
+    user='wwlffnpdbvfydz',
+    port='5432',
+    password='6605220694f6f57e65406f21956bf1f4f5b91803ac39a98e111a02e4c93ae3e6'
+)
+print(1231231)
+print(33)
+print('Hello!')
+
