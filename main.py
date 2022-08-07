9982a135d75b29647fde3088fd58f868c138bd2b
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-07 22:40:15 +0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-data = {'name': 'Fara',
-        'surname': 'Tutkabaev'}
-
-
-r = requests.post('http://127.0.0.1:5000/webhook', data=json.dumps(data), headers={'Content-Type': 'application/json'})
