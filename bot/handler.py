f2bd43aa88108091a8b8605f4499c1c2bb9e43d5
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-08 22:08:34 +0500

    Add files via upload

--- bot/handler.py
+++ bot/handler.py
@@ -25,16 +25,13 @@ def handler(body, x_line_signature):
     print(users.keys())
     if not user_id in users.keys():
         msg = body['events'][0]['message']['text']
-        if type_ == 'text' and msg.lower() == '/register':  
-            users[user_id] = 1
+        if type_ == 'text' and msg.lower() == '/register': 
+            users[user_id] = {} 
+            users[user_id]['count'] = 1
             line_bot_api.reply_message(reply_token, TextSendMessage(text='OK. Let\'s begin the registration.\n\nPlease tell use your full name (ex.: Stiven Gerrard):'))
-            print(users)
-            print()
-            print()
-            return 
+
         else:
             line_bot_api.reply_message(reply_token, TextSendMessage(text='Please, press "Registration" in the menu'))
-            return 
         return 
     # if not verify(body, x_line_signature):
     #     return False
@@ -43,7 +40,7 @@ def handler(body, x_line_signature):
     
     
     
-    if users[user_id] == 1:
+    if users[user_id]['count'] == 1:
         if type_ != 'text':
             line_bot_api.reply_message(reply_token, TextSendMessage(text='Please tell use your full name'))
         else:
@@ -52,15 +49,16 @@ def handler(body, x_line_signature):
                 line_bot_api.reply_message(reply_token, TextSendMessage(text='Please tell us your full name'))
             elif len(name.split(' ')) == 2:
                 line_bot_api.reply_message(reply_token, TextSendMessage(text=f'To finish the registration, please share your home location.'))
-                users[user_id] += 1
+                users[user_id]['count'] += 1
+                users[user_id]['name'] = name
     
-    elif users[user_id] == 2:
+    elif users[user_id]['count'] == 2:
         if type_ != 'location':
             line_bot_api.reply_message(reply_token, TextSendMessage(text='Please share your home location'))
         elif user_id in users.keys():
             address = body['events'][0]['message']['address']
-            line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Registration finished!\n\nThank you! It is nice to meet you {name}!\n! Now we know where you live... Good luck!\n{address}'))
-            users[user_id] += 1
+            line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Registration finished!\n\nThank you! It is nice to meet you {users[user_id]["name"]}!\n! Now we know where you live... Good luck!\n{address}'))
+            users[user_id]['count'] += 1
             del users[user_id]
    
 
