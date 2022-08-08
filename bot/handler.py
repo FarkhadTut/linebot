5233883e29331c1f3d3ba79d7665ec029d27f44d
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-08 22:40:04 +0500

    Add files via upload

--- bot/handler.py
+++ bot/handler.py
@@ -20,8 +20,15 @@ def handler(body, x_line_signature):
     user_id = body['events'][0]['source']['userId']
     reply_token = body['events'][0]['replyToken']
     type_ = body['events'][0]['message']['type']
-    print('User ID:', user_id)
-    print(users.keys())
+    print()
+
+    print()
+    print(users)
+    print()
+    print()
+    print()
+    print()
+
     if not user_id in users.keys():
         
         if type_ == 'text': 
