94052783fde62eb20a531b8a0684fe8369a5ba26
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-08 20:41:03 +0500

    Add files via upload

--- bot/handler.py
+++ bot/handler.py
@@ -20,14 +20,17 @@ def handler(body, x_line_signature):
 
     user_id = body['events'][0]['source']['userId']
     reply_token = body['events'][0]['replyToken']
-
+    type_ = body['events'][0]['message']['type']
     if not user_id in users.keys():
-        users[user_id] = 1
+        msg = body['events'][0]['message']['text']
+        if type_ == 'text' and msg.lower() == 'start':  
+            line_bot_api.reply_message(reply_token, TextSendMessage(text='OK. We are about to start a short registration process...'))
+            users[user_id] = 1
     # if not verify(body, x_line_signature):
     #     return False
     
     #logic
-    type_ = body['events'][0]['message']['type']
+    
     
     
     if users[user_id] == 1:
@@ -35,8 +38,11 @@ def handler(body, x_line_signature):
             line_bot_api.reply_message(reply_token, TextSendMessage(text='Please tell use your full name'))
         elif user_id in users.keys():
             name = body['events'][0]['message']['text']
-            line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Thank you! It is nice to meet you {name}!\nTo finish the registration, please share your home location.'))
-            users[user_id] += 1
+            if not ' ' in name or len(name.split(' ')) != 2:
+                line_bot_api.reply_message(reply_token, TextSendMessage(text='Please tell use your full name'))
+            else:
+                line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Thank you! It is nice to meet you {name}!\n\nTo finish the registration, please share your home location.'))
+                users[user_id] += 1
     
     elif users[user_id] == 2:
         if type_ != 'location':
@@ -44,6 +50,7 @@ def handler(body, x_line_signature):
         elif user_id in users.keys():
             address = body['events'][0]['message']['address']
             line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Thank you! Now we now where you live... Good luck!\n{address}'))
+            line_bot_api.reply_message(reply_token, TextSendMessage(text='Registration finished!'))
 
         
     
