09096b2f9dc34073f83f6123072515d8c79f8a89
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-08 20:27:10 +0500

    Add files via upload

--- bot/handler.py
+++ bot/handler.py
@@ -18,8 +18,8 @@ def handler(body, x_line_signature):
 
     # print(body['message'])
 
-    user_id = body['events']['message']['source']['userId']
-    reply_token = body['events']['replyToken']
+    user_id = body['events'][0]['message']['source']['userId']
+    reply_token = body['events'][0]['replyToken']
 
     if not user_id in users.keys():
         users[user_id] = 1
@@ -27,14 +27,14 @@ def handler(body, x_line_signature):
     #     return False
     
     #logic
-    type_ = body['events']['message']['type']
+    type_ = body['events'][0]['message']['type']
     
     
     if users[user_id] == 1:
         if type_ != 'text':
             line_bot_api.reply_message(reply_token, TextSendMessage(text='Please tell use your full name'))
         elif user_id in users.keys():
-            name = body['events']['message']['text']
+            name = body['events'][0]['message']['text']
             line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Thank you! It is nice to meet you {name}!\nTo finish the registration, please share your home location.'))
             users[user_id] += 1
     
@@ -42,7 +42,7 @@ def handler(body, x_line_signature):
         if type_ != 'location':
             line_bot_api.reply_message(reply_token, TextSendMessage(text='Please share your home location'))
         elif user_id in users.keys():
-            address = body['events']['message']['address']
+            address = body['events'][0]['message']['address']
             line_bot_api.reply_message(reply_token, TextSendMessage(text=f'Thank you! Now we now where you live... Good luck!\n{address}'))
 
         
